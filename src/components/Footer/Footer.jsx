5ba394eeb9cfaa2910f0ee70f92c3e236272cbc0
sileshino <sileshinooo@gmail.com>
2025-02-13 20:35:45 +0300

    fotter modified

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,28 +8,41 @@ const Footer = () => {
   return (
     <div className="footer_outer_container">
       <div className="footer_inner_container">
-        {/* Social Media Icons */}
-        <div className="footer_icons">
-          <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-            <FacebookOutlinedIcon className="icon" />
-          </a>
-          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-            <InstagramIcon className="icon" />
-          </a>
-          <a href="https://youtube.com" target="_blank" rel="noopener noreferrer">
-            <YouTubeIcon className="icon" />
-          </a>
+        {/* Left Column */}
+        <div className="footer_left_column">
+          {/* Social Media Icons */}
+          <div className="footer_icons">
+            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+              <FacebookOutlinedIcon className="icon" />
+            </a>
+            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+              <InstagramIcon className="icon" />
+            </a>
+            <a href="https://youtube.com" target="_blank" rel="noopener noreferrer">
+              <YouTubeIcon className="icon" />
+            </a>
+          </div>
+
+          {/* Links */}
+          <div className="footer_links">
+            <a href="/audio-description">Audio Description</a>
+            <a href="/investor-relations">Investor Relations</a>
+            <a href="/legal-notices">Legal Notices</a>
+          </div>
+
+          {/* Service Code */}
+          <div className="service_code">
+            <p>Service Code</p>
+          </div>
+
+          {/* Copyright Info */}
+          <div className="copy-write">
+            &copy; 1997-2024 Netflix, Inc.
+          </div>
         </div>
 
         {/* Footer Links */}
         <div className="footer_data">
-          <div>
-            <ul>
-              <li><a href="/audio-description">Audio Description</a></li>
-              <li><a href="/investor-relations">Investor Relations</a></li>
-              <li><a href="/legal-notices">Legal Notices</a></li>
-            </ul>
-          </div>
           <div>
             <ul>
               <li><a href="/help-center">Help Center</a></li>
@@ -51,16 +64,6 @@ const Footer = () => {
               <li><a href="/contact-us">Contact Us</a></li>
             </ul>
           </div>
-        </div>
-
-        {/* Service Code */}
-        <div className="service_code">
-          <p>Service Code</p>
-        </div>
-
-        {/* Copyright Info */}
-        <div className="copy-write">
-          &copy; 1997-2024 Netflix, Inc.
         </div>
       </div>
     </div>
